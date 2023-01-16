8eb69a4bc867e768aedc39499b5c9a53f20fdac4
Mokytojas <Mokytojas@kitm.local>
2023-01-16 10:55:06 +0200

    done

--- js.js
+++ js.js
@@ -14,7 +14,32 @@ const getCountryInfo = async (event) => {
         const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
         const data = await res.json();
         console.log(data);
-        
+        //tikrinti, ar salies info jau yra, pasalinti sena informacija   
+        if (document.querySelector('.one_country_info') != undefined) {
+            const countries = document.querySelectorAll('.one_country_info');
+            console.log(countries);
+            for (let i = 0; i < countries.length; i++) {
+                console.log(countries[i])
+                countries[i].remove();
+            }
+        }
+        //jei gauciau response daugiau ne viena sali
+        data.map(one_country => {
+            const one_country_info = document.createElement('div');
+            one_country_info.className = 'one_country_info card row d-flex justify-content-center mt-4';
+            one_country_info.innerHTML = `
+            <img src="${one_country.flags.png}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${one_country.name.common}</h5>
+                <p class="card-text">Population: ${one_country.capital[0]
+                }</p>
+                <p class="card-text">Population: ${one_country.population}</p>
+         
+       
+        </div>
+        `
+            document.querySelector('.container').appendChild(one_country_info)
+        })
     } catch (error) {
         console.log(error);
     }
